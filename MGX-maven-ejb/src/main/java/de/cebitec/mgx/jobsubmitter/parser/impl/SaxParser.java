5dd05d09f17c0341fd9ea75e62512d845e97592e
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2012-05-23 14:12:22 +0000

    JobSubmitter arbeitet mit Files statt Strings. Weitere Logger hinzugefuegt.
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@499 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/java/de/cebitec/mgx/jobsubmitter/parser/impl/SaxParser.java
+++ MGX-maven-ejb/src/main/java/de/cebitec/mgx/jobsubmitter/parser/impl/SaxParser.java
@@ -6,8 +6,6 @@
 package de.cebitec.mgx.jobsubmitter.parser.impl;
 
 //~--- non-JDK imports --------------------------------------------------------
-
-
 import de.cebitec.mgx.jobsubmitter.data.impl.Store;
 import de.cebitec.mgx.jobsubmitter.parser.documenthandler.PluginDocumentHandler;
 import de.cebitec.mgx.jobsubmitter.parser.documenthandler.ToolDocumentHandler;
@@ -16,6 +14,7 @@ import org.xml.sax.SAXException;
 //~--- JDK imports ------------------------------------------------------------
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.logging.Level;
@@ -34,109 +33,136 @@ import org.xml.sax.InputSource;
  */
 public class SaxParser {
 
-   private final static Logger LOGGER =
-	 Logger.getLogger(SaxParser.class.getName());
-   /**
-    * Der Parser für die jeweiligen Dateien.
-    */
-   private SAXParser parser;
-   /**
-    * Die Plugin Datei, wo alle möglichen Knoten aufgelistet sind
-    */
-   private String pluginFile;
-   /**
-    * EventHandler für die Plugin Datei.
-    *
-    */
-   private PluginDocumentHandler pluginHandler;
-   /**
-    * Store für die einzelnen Knoten.
-    */
-   private Store store;
-   /**
-    * Tool Datei, die vom User erstellt wurde.
-    */
-   private String toolFile;
-   /**
-    * Handler, der die Events, die beim Parsen der Tool Datei entstehen abfängt.
-    */
-   private ToolDocumentHandler toolHandler;
+    private final static Logger LOGGER =
+            Logger.getLogger(SaxParser.class.getName());
+    /**
+     * Der Parser für die jeweiligen Dateien.
+     */
+    private SAXParser parser;
+    /**
+     * Die Plugin Datei, wo alle möglichen Knoten aufgelistet sind
+     */
+    private File pluginFile;
+    /**
+     * EventHandler für die Plugin Datei.
+     *
+     */
+    private PluginDocumentHandler pluginHandler;
+    /**
+     * Store für die einzelnen Knoten.
+     */
+    private Store store;
+    /**
+     * Tool Datei, die vom User erstellt wurde.
+     */
+    private File toolFile;
+    /**
+     * Handler, der die Events, die beim Parsen der Tool Datei entstehen
+     * abfängt.
+     */
+    private ToolDocumentHandler toolHandler;
 
-   /**
-    * Konstruktor, der den Store und den Parser initialisiert.
-    *
-    *
-    * @param toolXml Die vom User erstellte Tool Datei.
-    * @param pluginsXml Die Plugins Datei mit allen möglichen Knoten.
-    */
-   public SaxParser() {
-	store = new Store();
+    /**
+     * Konstruktor, der den Store und den Parser initialisiert.
+     *
+     *
+     * @param toolXml Die vom User erstellte Tool Datei.
+     * @param pluginsXml Die Plugins Datei mit allen möglichen Knoten.
+     */
+    public SaxParser() {
+        store = new Store();
 
-	try {
-	   parser = SAXParserFactory.newInstance().newSAXParser();
-	} catch (ParserConfigurationException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
-		 ex);
-	} catch (SAXException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
-		 ex);
-	}
-   }
+        try {
+            parser = SAXParserFactory.newInstance().newSAXParser();
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+    }
 
-   /**
-    *
-    * Gibt die konfigurierbaren Nodes mit ihren ConfigItems wieder.
-    *
-    * @param toolXml XML Datei mit den vom User zusammengestellten Tools.
-    * @param pluginsXml Beinhaltet alle möglichen Nodes.
-    * @return NodeStore mit allen konfigurierbaren Knoten.
-    */
-   public Store getNodesConfigurations(String toolXml,
-	 String pluginsXml) {
-	
-	store.removeAllNodes();
-	toolFile = toolXml;
-	pluginFile = pluginsXml;
-	parse();
+    /**
+     *
+     * Gibt die konfigurierbaren Nodes mit ihren ConfigItems wieder.
+     *
+     * @param toolXml XML Datei mit den vom User zusammengestellten Tools.
+     * @param pluginsXml Beinhaltet alle möglichen Nodes.
+     * @return NodeStore mit allen konfigurierbaren Knoten.
+     */
+    public Store getNodesConfigurations(String toolXml,
+            String pluginsXml) {
 
+        store.removeAllNodes();
+        toolFile = new File(toolXml);
+        LOGGER.info("TOOL "+toolXml);
+        loadData(toolXml);
+        pluginFile = new File(pluginsXml);
+        LOGGER.info("Plugin "+pluginsXml);
+        parse();
 
-	return store;
-   }
+        return store;
+    }
 
-   /**
-    * In der Methode wird die Tool-Datei als auch die Plugin-Datei geparst.
-    *
-    */
-   private Store parse() {
-	toolHandler = new ToolDocumentHandler(store);
+      private static void loadData(String datName) {
 
-	try {
-           
-	   parser.parse(new InputSource( new StringReader(toolFile)), toolHandler);
-	} catch (SAXException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(
-		 Level.SEVERE,
-		 "Die XML-Datei des Tools konnte nicht geparst werden", ex);
-	} catch (IOException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
-		 ex);
-	}
+        File file = new File(datName);
 
-	pluginHandler =
-	    new PluginDocumentHandler(toolHandler.getFilledStore());
+        if (!file.canRead() || !file.isFile())
+            System.exit(0);
 
-	try {
-	   parser.parse(new InputSource( new StringReader(pluginFile)), pluginHandler);
-	} catch (SAXException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(
-		 Level.SEVERE,
-		 "Die XML-Datei des Plugins konnte nicht geparst werden", ex);
-	} catch (IOException ex) {
-	   Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
-		 ex);
-	}
-	return pluginHandler.getFilledStore();
-   }
+        FileReader fr = null;
+        int c;
+        StringBuffer buff = new StringBuffer();
+        try {
+            fr = new FileReader(file);
+            while ((c = fr.read()) != -1) {
+                buff.append((char) c);
+            }
+            fr.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(buff.toString());
+    } 
+    
+    
+    /**
+     * In der Methode wird die Tool-Datei als auch die Plugin-Datei geparst.
+     *
+     */
+    private Store parse() {
+        toolHandler = new ToolDocumentHandler(store);
+
+        try {
+            parser.parse(toolFile, toolHandler);
+        } catch (SAXException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(
+                    Level.SEVERE,
+                    "Die XML-Datei des Tools konnte nicht geparst werden", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+
+        pluginHandler =
+                new PluginDocumentHandler(toolHandler.getFilledStore());
+
+        try {
+            parser.parse(pluginFile, pluginHandler);
+        } catch (SAXException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(
+                    Level.SEVERE,
+                    "Die XML-Datei des Plugins konnte nicht geparst werden", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SAXParser.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+        return pluginHandler.getFilledStore();
+    }
 }
 
 
