a5921956a4658efe30b2fb2bfe7327555c994b9a
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2012-07-31 13:43:50 +0000

    sysout fuer parameter attribute
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@734 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/java/de/cebitec/mgx/jobsubmitter/JobParameterHelper.java
+++ MGX-maven-ejb/src/main/java/de/cebitec/mgx/jobsubmitter/JobParameterHelper.java
@@ -30,13 +30,37 @@ public class JobParameterHelper {
         } catch (ParserConfigurationException | SAXException | IOException ex) {
             Logger.getLogger(JobParameterHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
         LOGGER.info("StoreSize: " + store.storeSize() + "");
-
+        
         List<JobParameter> parameters = Transform.getFromNodeStoreJobParameter(store);
 
         LOGGER.info("ParameterSize: " + parameters.size() + "");
 
+        for(JobParameter parameter :  parameters){
+        
+            LOGGER.info("ID: "+parameter.getNodeId());
+            LOGGER.info("UserName: "+parameter.getUserName());
+            LOGGER.info("UserDescription: "+parameter.getUserDescription());
+            LOGGER.info("DisplayName: "+parameter.getDisplayName());
+            LOGGER.info("ClassName: "+parameter.getClassName());
+            LOGGER.info("ConfigItemName: "+parameter.getConfigItemName());
+            LOGGER.info("Type: "+parameter.getType());
+            LOGGER.info("Optional: "+parameter.isOptional());
+       
+//                .setNodeId(p.getNodeId())
+//                .setUserName(p.getUserName())
+//                .setUserDesc(p.getUserDescription())
+//                .setDisplayName(p.getDisplayName())
+//                .setClassName(p.getClassName())
+//                .setConfigitemName(p.getConfigItemName())
+//                .setType(p.getType())
+//                .setIsOptional(p.isOptional());
+        
+        }
+        
+        
+        
         return parameters;
     }
 }
