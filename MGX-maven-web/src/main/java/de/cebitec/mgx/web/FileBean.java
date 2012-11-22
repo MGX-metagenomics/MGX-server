6f4fbf0213e59ace62ef4b5b35d474b7dfb9e75c
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-11-22 19:02:49 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@983 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/FileBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/FileBean.java
@@ -5,16 +5,20 @@ import de.cebitec.mgx.controller.MGXController;
 import de.cebitec.mgx.dto.dto.FileDTO;
 import de.cebitec.mgx.dto.dto.FileDTOList;
 import de.cebitec.mgx.dto.dto.FileDTOList.Builder;
+import de.cebitec.mgx.dto.dto.MGXLong;
 import de.cebitec.mgx.web.exception.MGXWebException;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -36,6 +40,53 @@ public class FileBean {
         return listDir(currentDirectory);
     }
 
+    @PUT
+    @Path("create")
+    @Produces("application/x-protobuf")
+    public MGXLong create(FileDTO dto) {
+        // this method is only used to create directories
+        if (!dto.getIsDirectory()) {
+            throw new MGXWebException("Invalid input, only directories supported.");
+        }
+
+        // security check
+        if (dto.getName().contains("..") || !dto.getName().startsWith("." + File.separator)) {
+            mgx.log(mgx.getCurrentUser() + " tried to create " + dto.getName());
+            throw new MGXWebException("Invalid path.");
+        }
+
+        File target = new File(mgx.getProjectDirectory() + "files" + File.separatorChar + dto.getName());
+        if (target.exists()) {
+            throw new MGXWebException(dto.getName() + " already exists.");
+        }
+        if (!target.mkdir()) {
+            throw new MGXWebException("Could not create " + dto.getName());
+        }
+        return MGXLong.newBuilder().setValue(1).build();
+    }
+
+    @DELETE
+    @Path("delete/{path}")
+    public Response delete(@PathParam("path") String path) {
+
+        path = path.replace("|", "/");
+
+        // security check
+        if (path.contains("..") || !path.startsWith("." + File.separator)) {
+            mgx.log(mgx.getCurrentUser() + " tried to delete " + path);
+            throw new MGXWebException("Invalid path.");
+        }
+
+        File target = new File(mgx.getProjectDirectory() + "files" + File.separatorChar + path);
+        if (!target.exists()) {
+            throw new MGXWebException("Nonexisting path: " + path);
+        }
+
+        deleteHierarchy(target);
+        return Response.ok().build();
+    }
+
+
     private FileDTOList listDir(File base) {
         //
         // This is ugly - application logic shouldn't exist on the
@@ -54,9 +105,9 @@ public class FileBean {
         }
         return list.build();
     }
-    
+
     private File getCurrentDirectory(String path) {
-        
+
         path = path.replace("|", "/");
 
         if (path.contains("..")) {
@@ -78,6 +129,16 @@ public class FileBean {
             throw new MGXWebException(basedir.getAbsolutePath() + " is not a directory.");
         }
         return basedir;
+    }
+    
+    
+    private void deleteHierarchy(File f) {
+        if (f.isDirectory()) {
+            for (File tmp : f.listFiles()) {
+                deleteHierarchy(tmp);
+            }
+        }
+        f.delete();
     }
 
     private String stripProjectDir(String input) {
