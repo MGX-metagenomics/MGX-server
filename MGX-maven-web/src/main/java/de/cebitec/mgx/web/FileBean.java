34e299dc27851e6d98c29911e7167b5c56debca2
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-11-28 15:03:16 +0000

    working version of file upload
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@994 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/FileBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/FileBean.java
@@ -2,18 +2,27 @@ package de.cebitec.mgx.web;
 
 import de.cebitec.mgx.controller.MGX;
 import de.cebitec.mgx.controller.MGXController;
+import de.cebitec.mgx.controller.MGXException;
+import de.cebitec.mgx.dto.dto.BytesDTO;
 import de.cebitec.mgx.dto.dto.FileDTO;
 import de.cebitec.mgx.dto.dto.FileDTOList;
 import de.cebitec.mgx.dto.dto.FileDTOList.Builder;
 import de.cebitec.mgx.dto.dto.MGXLong;
+import de.cebitec.mgx.dto.dto.MGXString;
+import de.cebitec.mgx.upload.FileUploadReceiver;
+import de.cebitec.mgx.upload.UploadSessions;
 import de.cebitec.mgx.web.exception.MGXWebException;
 import java.io.File;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -31,6 +40,8 @@ public class FileBean {
     @Inject
     @MGX
     MGXController mgx;
+    @EJB(lookup = "java:global/MGX-maven-ear/MGX-maven-web/UploadSessions")
+    UploadSessions sessions;
 
     @GET
     @Path("fetchall/{baseDir}")
@@ -86,6 +97,65 @@ public class FileBean {
         return Response.ok().build();
     }
 
+    @GET
+    @Path("init/{path}")
+    @Produces("application/x-protobuf")
+    public MGXString init(@PathParam("path") String path) {
+
+        mgx.log("Creating file upload session for " + mgx.getProjectName());
+        FileUploadReceiver recv = null;
+
+        path = path.replace("|", "/");
+
+        // security check
+        if (path.contains("..") || !path.startsWith("." + File.separator)) {
+            mgx.log(mgx.getCurrentUser() + " tried to access " + path);
+            throw new MGXWebException("Invalid path.");
+        }
+
+        File target = new File(mgx.getProjectDirectory() + "files" + File.separatorChar + path);
+        if (target.exists()) {
+            throw new MGXWebException("File already exists: " + path);
+        }
+
+        recv = new FileUploadReceiver(target.getAbsolutePath(), mgx.getProjectName());
+        UUID uuid = sessions.registerUploadSession(recv);
+        return MGXString.newBuilder().setValue(uuid.toString()).build();
+    }
+
+    @GET
+    @Path("close/{uuid}")
+    public Response close(@PathParam("uuid") UUID session_id) {
+        try {
+            sessions.closeSession(session_id);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ex.getMessage());
+        }
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("add/{uuid}")
+    @Consumes("application/x-protobuf")
+    public Response add(@PathParam("uuid") UUID session_id, BytesDTO data) {
+        try {
+            sessions.getSession(session_id).add(data.getData().toByteArray());
+        } catch (MGXException ex) {
+            throw new MGXWebException(ex.getMessage());
+        }
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("cancel/{uuid}")
+    public Response cancel(@PathParam("uuid") UUID session_id) {
+        try {
+            sessions.cancelSession(session_id);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ex.getMessage());
+        }
+        return Response.ok().build();
+    }
 
     private FileDTOList listDir(File base) {
         //
@@ -130,8 +200,7 @@ public class FileBean {
         }
         return basedir;
     }
-    
-    
+
     private void deleteHierarchy(File f) {
         if (f.isDirectory()) {
             for (File tmp : f.listFiles()) {
