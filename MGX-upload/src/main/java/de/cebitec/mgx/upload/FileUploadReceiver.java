2a91554b22f2a19330cb5b57301a0b588253bf7c
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-11-28 15:04:18 +0000

    working version of file upload
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@997 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-upload/src/main/java/de/cebitec/mgx/upload/FileUploadReceiver.java
@@ -0,0 +1,86 @@
+package de.cebitec.mgx.upload;
+
+import de.cebitec.mgx.controller.MGXException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+/**
+ *
+ * @author sjaenick
+ */
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class FileUploadReceiver implements UploadReceiverI<byte[]> {
+
+    protected FileOutputStream writer = null;
+    protected final String targetFile;
+    protected final String projectName;
+    protected long lastAccessed;
+
+    public FileUploadReceiver(String target, String projectName) {
+        this.targetFile = target;
+        this.projectName = projectName;
+        lastAccessed = System.currentTimeMillis();
+    }
+
+    @Override
+    public void cancel() {
+        if (writer != null) {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+            } finally {
+                writer = null;
+            }
+        }
+        File file = new File(targetFile);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        lastAccessed = System.currentTimeMillis();
+    }
+
+    @Override
+    public void close() throws MGXException {
+        try {
+            writer.close();
+        } catch (IOException ex) {
+            throw new MGXException(ex);
+        } finally {
+            writer = null;
+        }
+        lastAccessed = System.currentTimeMillis();
+    }
+
+    @Override
+    public void add(byte[] data) throws MGXException {
+        if (writer == null) {
+            try {
+                writer = new FileOutputStream(targetFile);
+            } catch (IOException ex) {
+                throw new MGXException(ex.getMessage());
+            }
+        }
+
+        try {
+            writer.write(data);
+        } catch (IOException ex) {
+            cancel();
+            throw new MGXException(ex);
+        }
+        lastAccessed = System.currentTimeMillis();
+    }
+
+    @Override
+    public String getProjectName() {
+        return projectName;
+    }
+
+    @Override
+    public long lastAccessed() {
+        return lastAccessed;
+    }
+}
