d917340e665557fc284924bd769065303c0efd34
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-01-07 16:09:36 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1044 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqReaderI.java
+++ MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqReaderI.java
@@ -1,4 +1,3 @@
-
 package de.cebitec.mgx.sequence;
 
 import java.util.Enumeration;
@@ -9,8 +8,8 @@ import java.util.Set;
  * @author sjaenick
  */
 public interface SeqReaderI<T> extends Enumeration<T>, AutoCloseable {
-    
+
     public void delete();
+
     public Set<T> fetch(long[] ids) throws SeqStoreException;
-    
 }
