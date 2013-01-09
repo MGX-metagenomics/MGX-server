a2f8d2f59ce1d460efd2c9bb8354c02228be62a7
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-01-09 12:31:22 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1052 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqWriterI.java
+++ MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqWriterI.java
@@ -6,9 +6,8 @@ import java.io.IOException;
  *
  * @author sjaenick
  */
-public interface SeqWriterI<T extends DNASequenceI> {
+public interface SeqWriterI<T extends DNASequenceI> extends AutoCloseable {
     
     void addSequence(T seq) throws IOException;
 
-    void close() throws IOException;
 }
