f5f6338ee42b1198dba06f73ec05e8f0680f0090
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-06-20 12:50:47 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1382 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/AttributeTypeBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/AttributeTypeBean.java
@@ -2,8 +2,16 @@ package de.cebitec.mgx.web;
 
 import de.cebitec.mgx.controller.MGX;
 import de.cebitec.mgx.controller.MGXController;
+import de.cebitec.mgx.controller.MGXException;
+import de.cebitec.mgx.dto.dto;
+import de.cebitec.mgx.dto.dto.AttributeTypeDTO;
 import de.cebitec.mgx.dto.dto.AttributeTypeDTOList;
+import de.cebitec.mgx.dtoadapter.AttributeDTOFactory;
 import de.cebitec.mgx.dtoadapter.AttributeTypeDTOFactory;
+import de.cebitec.mgx.model.db.Attribute;
+import de.cebitec.mgx.model.db.AttributeType;
+import de.cebitec.mgx.web.exception.MGXWebException;
+import de.cebitec.mgx.web.helper.ExceptionMessageConverter;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -22,6 +30,19 @@ public class AttributeTypeBean {
     @Inject
     @MGX
     MGXController mgx;
+
+    @GET
+    @Path("fetch/{id}")
+    @Produces("application/x-protobuf")
+    public AttributeTypeDTO fetch(@PathParam("id") Long id) {
+        AttributeType obj = null;
+        try {
+            obj = mgx.getAttributeTypeDAO().getById(id);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ExceptionMessageConverter.convert(ex.getMessage()));
+        }
+        return AttributeTypeDTOFactory.getInstance().toDTO(obj);
+    }
 
     @GET
     @Path("ByJob/{jobId}")
