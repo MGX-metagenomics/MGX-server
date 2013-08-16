94332cd2c737ad87cf55e81cb1ef141b3e3c492f
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-08-16 09:49:10 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1449 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/java/de/cebitec/mgx/model/db/Reference.java
+++ MGX-maven-ejb/src/main/java/de/cebitec/mgx/model/db/Reference.java
@@ -80,4 +80,8 @@ public class Reference implements Serializable, Identifiable {
     public Long getId() {
      return id;   
     }
+    
+    public void setId(Long l) {
+        id = l;
+    }
 }
