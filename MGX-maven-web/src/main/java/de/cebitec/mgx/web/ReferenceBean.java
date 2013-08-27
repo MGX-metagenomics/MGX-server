42a6f4d40b9718a935fa508f1f498fd1b0decbeb
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2013-08-27 18:12:17 +0000

    Reference Bean
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1453 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/ReferenceBean.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.web;
+
+import de.cebitec.gpms.security.Secure;
+import de.cebitec.mgx.controller.MGX;
+import de.cebitec.mgx.controller.MGXController;
+import de.cebitec.mgx.controller.MGXException;
+import de.cebitec.mgx.controller.MGXRoles;
+import de.cebitec.mgx.dto.dto;
+import de.cebitec.mgx.dto.dto.MGXLong;
+import de.cebitec.mgx.dtoadapter.DNAExtractDTOFactory;
+import de.cebitec.mgx.dtoadapter.ReferenceDTOFactory;
+import de.cebitec.mgx.model.dao.deleteworkers.DeleteDNAExtract;
+import de.cebitec.mgx.model.db.DNAExtract;
+import de.cebitec.mgx.model.db.Reference;
+import de.cebitec.mgx.model.db.Sample;
+import de.cebitec.mgx.sessions.TaskHolder;
+import de.cebitec.mgx.web.exception.MGXWebException;
+import de.cebitec.mgx.web.helper.ExceptionMessageConverter;
+import java.util.UUID;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author belmann
+ */
+@Stateless
+@Path("Reference")
+public class ReferenceBean {
+   
+    
+    @Inject
+    @MGX
+    MGXController mgx;
+
+    @PUT
+    @Path("create")
+    @Consumes("application/x-protobuf")
+    @Produces("application/x-protobuf")
+    public dto.MGXLong create(dto.ReferenceDTO dto) {
+        Long Reference_id = null;
+        try {
+            //Sample s = mgx.getSampleDAO().getById(dto.getSampleId());
+            Reference ref =  ReferenceDTOFactory.getInstance().toDB(dto);
+
+            Reference_id = mgx.getReferenceDAO().create(ref);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ExceptionMessageConverter.convert(ex.getMessage()));
+        }
+        return MGXLong.newBuilder().setValue(Reference_id).build();
+    }
+    
+    
+    @POST
+    @Path("update")
+    @Consumes("application/x-protobuf")
+    public Response update(dto.ReferenceDTO dto) {
+       
+        Reference reference = ReferenceDTOFactory.getInstance().toDB(dto); 
+        try {
+            mgx.getReferenceDAO().update(reference);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ExceptionMessageConverter.convert(ex.getMessage()));
+        }
+        return Response.ok().build();
+    }
+    
+    
+    @GET
+    @Path("fetch/{id}")
+    @Produces("application/x-protobuf")
+    public dto.ReferenceDTO fetch(@PathParam("id") Long id) {
+        Reference obj = null;
+        try {
+            obj = (Reference) mgx.getReferenceDAO().getById(id);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ExceptionMessageConverter.convert(ex.getMessage()));
+        }
+        return ReferenceDTOFactory.getInstance().toDTO(obj);
+    }
+
+    @GET
+    @Path("fetchall")
+    @Produces("application/x-protobuf")
+    public dto.ReferenceDTOList fetchall() {
+        return ReferenceDTOFactory.getInstance().toDTOList(mgx.getReferenceDAO().getAll());
+    }
+}
