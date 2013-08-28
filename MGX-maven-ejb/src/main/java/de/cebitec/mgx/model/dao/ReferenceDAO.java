c55ec9d8a0bb72bb017ff8fb440f2968981769c7
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-08-28 13:54:07 +0000

    add fetching of regions by interval
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1464 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/java/de/cebitec/mgx/model/dao/ReferenceDAO.java
+++ MGX-maven-ejb/src/main/java/de/cebitec/mgx/model/dao/ReferenceDAO.java
@@ -1,7 +1,13 @@
-
 package de.cebitec.mgx.model.dao;
 
+import de.cebitec.mgx.controller.MGXException;
 import de.cebitec.mgx.model.db.Reference;
+import de.cebitec.mgx.model.db.Region;
+import de.cebitec.mgx.util.DBIterator;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -13,5 +19,35 @@ public class ReferenceDAO<T extends Reference> extends DAO<T> {
     public Class getType() {
         return Reference.class;
     }
-    
+
+    public DBIterator<Region> byReferenceInterval(long seqId, int from, int to) throws MGXException {
+        DBIterator<Region> iter = null;
+        Connection conn = getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rset = null;
+        try {
+            stmt = conn.prepareStatement("SELECT * from getRegions(?,?,?)");
+            stmt.setLong(1, seqId);
+            stmt.setInt(2, from);
+            stmt.setInt(3, to);
+            rset = stmt.executeQuery();
+
+            iter = new DBIterator<Region>(rset, stmt, conn) {
+                @Override
+                public Region convert(ResultSet rs) throws SQLException {
+                    Region r = new Region();
+                    r.setId(rs.getLong(1));
+                    r.setName(rs.getString(2));
+                    r.setDescription(rs.getString(3));
+                    r.setStart(rs.getInt(4));
+                    r.setStop(rs.getInt(5));
+                    return r;
+                }
+            };
+
+        } catch (SQLException ex) {
+            throw new MGXException(ex.getMessage());
+        }
+        return iter;
+    }
 }
