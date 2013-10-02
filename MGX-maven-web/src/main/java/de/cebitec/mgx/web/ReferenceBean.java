3a8870ffebc071be415f4fd0c7fdaf05d17cd392
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-02 12:47:34 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1549 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/ReferenceBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/ReferenceBean.java
@@ -26,8 +26,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
