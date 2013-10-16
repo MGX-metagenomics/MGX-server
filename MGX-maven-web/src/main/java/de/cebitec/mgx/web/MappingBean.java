338644846b907b7581c81a59d98dd2560d941447
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-16 17:35:54 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1624 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/MappingBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/MappingBean.java
@@ -3,21 +3,28 @@ package de.cebitec.mgx.web;
 import de.cebitec.mgx.controller.MGX;
 import de.cebitec.mgx.controller.MGXController;
 import de.cebitec.mgx.controller.MGXException;
+import de.cebitec.mgx.dto.dto.MGXString;
 import de.cebitec.mgx.dto.dto.MappingDTO;
 import de.cebitec.mgx.dto.dto.MappingDTOList;
 import de.cebitec.mgx.dtoadapter.MappingDTOFactory;
 import de.cebitec.mgx.model.db.Mapping;
 import de.cebitec.mgx.model.db.Reference;
 import de.cebitec.mgx.model.db.SeqRun;
+import de.cebitec.mgx.sessions.TaskHolder;
 import de.cebitec.mgx.util.AutoCloseableIterator;
 import de.cebitec.mgx.web.exception.MGXWebException;
 import de.cebitec.mgx.web.helper.ExceptionMessageConverter;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -30,6 +37,8 @@ public class MappingBean {
     @Inject
     @MGX
     MGXController mgx;
+    @EJB
+    TaskHolder taskHolder;
 
     @GET
     @Path("fetch/{id}")
@@ -77,5 +86,33 @@ public class MappingBean {
         }
         AutoCloseableIterator<Mapping> mappings = mgx.getMappingDAO().byReference(ref);
         return MappingDTOFactory.getInstance().toDTOList(mappings);
+    }
+
+    // access to mapping details, session-based
+    @GET
+    @Path("openMapping/{mapid}")
+    @Produces("application/x-protobuf")
+    public MGXString openMapping(@PathParam("mapid") Long mapid) {
+        UUID uuid = null;
+        try {
+            Mapping m = mgx.getMappingDAO().getById(mapid);
+        } catch (MGXException ex) {
+            Logger.getLogger(MappingBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return MGXString.newBuilder().setValue(uuid.toString()).build();
+    }
+
+    @GET
+    @Path("byReferenceInterval/{uuid}/{from}/{to}")
+    @Produces("application/x-protobuf")
+    public void byReferenceInterval(@PathParam("uuid") UUID uuid, @PathParam("from") int from, @PathParam("to") int to) {
+    }
+
+    @GET
+    @Path("closeMapping/{uuidid}")
+    @Produces("application/x-protobuf")
+    public Response closeMapping(@PathParam("uuid") UUID uuid) {
+        taskHolder.removeTask(uuid);
+        return Response.ok().build();
     }
 }
