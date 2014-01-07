f6c24bfbd38d209f489d66376afb59700960bf12
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-07 19:04:33 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1699 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/StatisticsBean.java
@@ -0,0 +1,54 @@
+package de.cebitec.mgx.web;
+
+import de.cebitec.gpms.security.Secure;
+import de.cebitec.mgx.controller.MGX;
+import de.cebitec.mgx.controller.MGXController;
+import de.cebitec.mgx.controller.MGXException;
+import de.cebitec.mgx.controller.MGXRoles;
+import de.cebitec.mgx.dto.dto.MGXLong;
+import de.cebitec.mgx.dto.dto.MGXLongList;
+import de.cebitec.mgx.dto.dto.PointDTOList;
+import de.cebitec.mgx.dtoadapter.PointDTOFactory;
+import de.cebitec.mgx.statistics.Rarefaction;
+import de.cebitec.mgx.util.AutoCloseableIterator;
+import de.cebitec.mgx.util.Point;
+import de.cebitec.mgx.web.exception.MGXWebException;
+import de.cebitec.mgx.web.helper.ExceptionMessageConverter;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author sj
+ */
+@Path("Statistics")
+@Stateless
+public class StatisticsBean {
+
+    @Inject
+    @MGX
+    MGXController mgx;
+
+    @EJB
+    Rarefaction rarefaction;
+
+    @PUT
+    @Path("Rarefaction")
+    @Consumes("application/x-protobuf")
+    @Produces("application/x-protobuf")
+    @Secure(rightsNeeded = {MGXRoles.Guest})
+    public PointDTOList create(MGXLongList dto) {
+        long[] data = new long[dto.getLongCount()];
+        for (int i=0;i< dto.getLongCount(); i++) {
+            data[i] = dto.getLong(i).getValue();
+        }
+        AutoCloseableIterator<Point> ret = rarefaction.rarefy(data);
+        return PointDTOFactory.getInstance().toDTOList(ret);
+    }
+}
