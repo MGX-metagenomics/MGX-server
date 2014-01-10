e9dfdd3372526816a322b8cf1156ce72de23e12f
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-10 15:35:56 +0000

    parallelize rarefaction()
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1720 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/resources/de/cebitec/mgx/statistics/Rfunctions.r
+++ MGX-maven-ejb/src/main/resources/de/cebitec/mgx/statistics/Rfunctions.r
@@ -1,90 +1,40 @@
-##This function creates true individual based rarefaction curves				###
-##Author: Joshua Jacobs   email@joshuajacobs.org  www.joshuajacobs.org/R/rarefaction.html	###
-##Usage rarefaction(x,y), x is the community matrix with species as columns			###
 
-rarefaction<-function(x,subsample=5, plot=TRUE, color=TRUE, error=FALSE, legend=TRUE, symbol=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)){
+rarefaction<-function(x, subsample=5, plot=TRUE, color=TRUE, error=FALSE, legend=TRUE, symbol=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) {
 
-        library(vegan)
+  library(vegan)
+  library(parallel)
 
-        x <- as.matrix(x)
-	y1<-apply(x, 1, sum)
-        rare.data<-x                                   
+  x <- as.matrix(x)
+  y1<-apply(x, 1, sum)
+  rare.data<-x                                   
 
-        select<-unique(sort(c((apply(x, 1, sum)), (seq(0,(max(y1)), by=subsample)), recursive=TRUE)))
+  select<-unique(sort(c((apply(x, 1, sum)), (seq(0,(max(y1)), by=subsample)), recursive=TRUE)))
 
-        storesummary.e<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
-        rownames(storesummary.e)<-c(select)
-        colnames(storesummary.e)<-rownames(x)
-        storesummary.se<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
-        rownames(storesummary.se)<-c(select)
-        colnames(storesummary.se)<-rownames(x)
+  storesummary.e<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
+  rownames(storesummary.e)<-c(select)
+  colnames(storesummary.e)<-rownames(x)
+  storesummary.se<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
+  rownames(storesummary.se)<-c(select)
+  colnames(storesummary.se)<-rownames(x)
 
-                for(i in 1:length(select))                      #the for loop
-                        {
-                        select.c<-select[i]                     #assigns the 'i'th element of select to select.c
-                        foo<-rarefy(x,select.c, se=T)           #use whatever vegan fn you want
-                                                        
-                storesummary.e[i,]<-foo[1,]
-                storesummary.se[i,]<-foo[2,]            
-                                        
-                        }
+  tmp <- mclapply(1:length(select), function(X) rareStep(x, select, X), mc.cores=48)
+  for (i in 1:length(select)) {
+    storesummary.e[i,] <- tmp[[i]][1]
+    storesummary.se[i,] <- tmp[[i]][2]
+  }
                 
-                storesummary.e<-as.data.frame(storesummary.e)               
-                richness.error<<-storesummary.se
+  storesummary.e<-as.data.frame(storesummary.e)               
+  richness.error<<-storesummary.se
                 
-for (i in 1:(length(storesummary.e)))
-{
-storesummary.e[,i]<-ifelse(select>sum(x[i,]), NA, storesummary.e[,i])
+  for (i in 1:(length(storesummary.e))) {
+    storesummary.e[,i]<-ifelse(select>sum(x[i,]), NA, storesummary.e[,i])
+  }
+
+  list("richness"= storesummary.e, "SE"=richness.error, "subsample"=select)        
 }
 
-
-
-###############plot result################################
-if (plot==TRUE)
- {
-if(color==TRUE){
-plot(select,storesummary.e[,1], xlab="Individuals in Subsample", 
-xlim=c(0,max(select)), ylim=c(0, 5+(max(storesummary.e[,1:(length(storesummary.e))], na.rm=TRUE))),
-ylab="Mean Species Richness", pch =16, col=2, type="n")
-
-for (j in 1:(length(storesummary.e))){
-points(select, storesummary.e[,j], pch=16, col=j+1, type="b", lty=1)}
-
-if(error==TRUE){
-for (m in 1:(length(storesummary.e))){
-segments(select, storesummary.e[,m]+storesummary.se[,m],select, storesummary.e[,m]-storesummary.se[,m])
+rareStep<-function(x, select, i) {
+  select.c<-select[i]
+  foo<-rarefy(x, select.c, se=T)
+  return(c(foo[1,], foo[2,]))
 }
-}
-if (legend==TRUE){
-legend("bottomright", colnames(storesummary.e), inset=0.05, lty=1, col=1:length(storesummary.e)+1, lwd=2)
-}
-}
-else
-{
-plot(select,storesummary.e[,1], xlab="Individuals in Subsample", 
-xlim=c(0,max(select)), ylim=c(0, 5+(max(storesummary.e[,1:(length(storesummary.e))], na.rm=TRUE))),
-ylab="Mean Species Richness", pch =16, col=2, type="n")
-
-for (j in 1:(length(storesummary.e))){
-points(select, storesummary.e[,j], type="l", lty=1)}
-
-for (k in 1:(length(storesummary.e))){
-symbol<-ifelse(symbol<length(storesummary.e),rep(symbol,2),symbol)
-points(as.numeric(rownames(subset(storesummary.e, storesummary.e[,k]==max(storesummary.e[,k],na.rm=TRUE)))), max(storesummary.e[,k],na.rm=TRUE), pch=symbol[k], cex=1.5)}
-
-if(error==TRUE){
-for (m in 1:(length(storesummary.e))){
-points(select, storesummary.e[,m]+storesummary.se[,m], type="l", lty=2)
-points(select, storesummary.e[,m]-storesummary.se[,m], type="l", lty=2)}}
-
-k<-1:(length(storesummary.e))
-if (legend==TRUE){
-legend("bottomright", colnames(storesummary.e), pch=symbol[k], inset=0.05, cex=1.3)
-}
-}
-}
-#print("rarefaction by J. Jacobs, last update April 17, 2009")
-if(error==TRUE)(print("errors around lines are the se of the iterations, not true se of the means")  )     
-list("richness"= storesummary.e, "SE"=richness.error, "subsample"=select)        
- 
-        }
