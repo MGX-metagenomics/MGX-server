070da185825dda13ae6c89b81646ab247d9cc844
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-04-23 12:05:04 +0000

    add matrix DTO conversion
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2142 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-DTOAdapter/src/main/java/de/cebitec/mgx/dtoadapter/MatrixDTOFactory.java
@@ -0,0 +1,64 @@
+package de.cebitec.mgx.dtoadapter;
+
+import de.cebitec.mgx.dto.dto.MGXMatrixDTO;
+import de.cebitec.mgx.dto.dto.MGXString;
+import de.cebitec.mgx.dto.dto.ProfileDTO;
+import de.cebitec.mgx.model.misc.Matrix;
+import de.cebitec.mgx.model.misc.NamedVector;
+import de.cebitec.mgx.util.AutoCloseableIterator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class MatrixDTOFactory extends DTOConversionBase<Matrix, MGXMatrixDTO, Object> {
+
+    static {
+        instance = new MatrixDTOFactory();
+    }
+    protected final static MatrixDTOFactory instance;
+
+    private MatrixDTOFactory() {
+    }
+
+    public static MatrixDTOFactory getInstance() {
+        return instance;
+    }
+
+    @Override
+    public MGXMatrixDTO toDTO(Matrix a) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Matrix toDB(MGXMatrixDTO dto) {
+        Set<NamedVector> data = new HashSet<>();
+        for (ProfileDTO pdto : dto.getRowList()) {
+            List<Long> ll = pdto.getValues().getLongList();
+            long[] tmp = new long[ll.size()];
+            for (int i = 0; i < tmp.length; i++) {
+                tmp[i] = ll.get(i);
+            }
+            data.add(new NamedVector(pdto.getName(), tmp));
+        }
+
+        String[] attrNames = null;
+        if (dto.hasColNames()) {
+            MGXString[] aa = dto.getColNames().getStringList().toArray(new MGXString[]{});
+            attrNames = new String[aa.length];
+            int i = 0;
+            for (MGXString ms : aa) {
+                attrNames[i++] = ms.getValue();
+            }
+        }
+        return new Matrix(attrNames, data);
+    }
+
+    @Override
+    public Object toDTOList(AutoCloseableIterator<Matrix> list) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+}
