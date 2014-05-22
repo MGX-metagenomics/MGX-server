85f5f8b67504b5ff1ca8634addcd6902cc0b58e7
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-05-22 15:20:04 +0000

    fix sam stuff
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2351 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-maven-ejb/src/main/java/de/cebitec/mgx/sessions/MappingDataSession.java
+++ MGX-maven-ejb/src/main/java/de/cebitec/mgx/sessions/MappingDataSession.java
@@ -5,10 +5,14 @@ import de.cebitec.mgx.util.AutoCloseableIterator;
 import de.cebitec.mgx.util.AutoCloseableSAMRecordIterator;
 import de.cebitec.mgx.model.misc.MappedSequence;
 import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
 import net.sf.samtools.SAMRecordIterator;
 
 /**
@@ -21,6 +25,7 @@ public class MappingDataSession {
     private final int refLength;
     private final String projName;
     private long lastAccessed;
+    private final String samFile;
     private final SAMFileReader samReader;
     private final Lock lock;
 
@@ -28,6 +33,7 @@ public class MappingDataSession {
         this.refId = refId;
         this.refLength = refLen;
         this.projName = projName;
+        this.samFile = samFile;
         lastAccessed = System.currentTimeMillis();
         samReader = new SAMFileReader(new File(samFile));
         lock = new ReentrantLock();
@@ -37,7 +43,8 @@ public class MappingDataSession {
         lastAccessed = System.currentTimeMillis();
         try {
             if (lock.tryLock(5, TimeUnit.SECONDS)) {
-                SAMRecordIterator overlaps = samReader.queryOverlapping(String.valueOf(refId), from, to);
+                // query 1-based
+                SAMRecordIterator overlaps = samReader.queryOverlapping(String.valueOf(refId), from + 1, to + 1);
                 return new AutoCloseableSAMRecordIterator(overlaps, lock);
             } else {
                 throw new MGXException("Failed to acquire lock, please try again later.");
@@ -46,7 +53,16 @@ public class MappingDataSession {
             throw new MGXException(ex);
         }
     }
-    
+
+    public long getMaxCoverage() {
+        ForkJoinPool pool = new ForkJoinPool();
+        // sam-jdk wants 1-based positions - ARGH
+        GetCoverage getCov = new GetCoverage(samFile, 1, refLength, refId);
+        long ret = pool.invoke(getCov);
+        pool.shutdown();
+        return ret;
+    }
+
     public long lastAccessed() {
         return lastAccessed;
     }
@@ -59,4 +75,68 @@ public class MappingDataSession {
         samReader.close();
     }
 
+    public static class GetCoverage extends RecursiveTask<Long> {
+
+        private final String samFile;
+        private final int from;  // 1-based
+        private final int to; // 1-based
+        private final long refId;
+
+        private final static int THRESHOLD = 5000;
+
+        /*
+         * 1-based positions !!!
+         */
+        public GetCoverage(String samFile, int from, int to, long refId) {
+            this.samFile = samFile;
+            this.from = from;
+            this.to = to;
+            this.refId = refId;
+        }
+
+        @Override
+        protected Long compute() {
+            int length = from - to + 1;
+
+            if (length > THRESHOLD) {
+                // split into two tasks
+                int mid = length / 2;
+                GetCoverage left = new GetCoverage(samFile, from, mid, refId);
+                left.fork();
+                GetCoverage right = new GetCoverage(samFile, mid + 1, to, refId);
+                right.compute();
+
+                long maxLeft = left.join();
+                long maxRight = right.compute();
+                return Math.max(maxLeft, maxRight);
+            }
+
+            int[] coverage = new int[to - from + 1];
+            Arrays.fill(coverage, 0);
+            SAMFileReader samReader = new SAMFileReader(new File(samFile));
+            SAMRecordIterator iter = samReader.queryOverlapping(String.valueOf(refId), from, to);
+            while (iter.hasNext()) {
+                SAMRecord record = iter.next();
+                assert record.getAlignmentStart() < record.getAlignmentEnd();
+
+                for (int i = record.getAlignmentStart(); i <= record.getAlignmentEnd(); i++) {
+                    // we need to check extra since we also receive mappings
+                    // which only partially overlap with the interval
+                    if (i >= from && i <= to) {
+                        coverage[i - from]++;
+                    }
+                }
+            }
+            iter.close();
+            samReader.close();
+            long max = 0;
+            for (int i : coverage) {
+                if (i > max) {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+    }
 }
