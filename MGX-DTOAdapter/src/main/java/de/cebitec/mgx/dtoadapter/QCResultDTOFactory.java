9a6aca61e0444be3d1cdc4ee544bedcf0283f5f3
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2014-12-01 17:38:53 +0100

    add QCResult DTO type & conversion

---
+++ MGX-DTOAdapter/src/main/java/de/cebitec/mgx/dtoadapter/QCResultDTOFactory.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.dtoadapter;
+
+import de.cebitec.mgx.dto.dto.DataRowDTO;
+import de.cebitec.mgx.dto.dto.QCResultDTO;
+import de.cebitec.mgx.dto.dto.QCResultDTOList;
+import de.cebitec.mgx.qc.DataRow;
+import de.cebitec.mgx.qc.QCResult;
+import de.cebitec.mgx.util.AutoCloseableIterator;
+
+/**
+ *
+ * @author sj
+ */
+public class QCResultDTOFactory extends DTOConversionBase<QCResult, QCResultDTO, QCResultDTOList> {
+
+    @Override
+    public QCResultDTO toDTO(QCResult a) { 
+        QCResultDTO.Builder b = QCResultDTO.newBuilder();
+        b.setName(a.getName());
+        for (DataRow dr : a.getData()) {
+            DataRowDTO.Builder drdb = DataRowDTO.newBuilder();
+            drdb.setName(dr.getName());
+            for (float f : dr.getData()) {
+                drdb.addValue(f);
+            }
+            b.addRow(drdb.build());
+        }
+        return b.build();
+    }
+
+    @Override
+    public QCResult toDB(QCResultDTO dto) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public QCResultDTOList toDTOList(AutoCloseableIterator<QCResult> list) {
+        QCResultDTOList.Builder b = QCResultDTOList.newBuilder();
+        while (list.hasNext()) {
+            b = b.addResult(toDTO(list.next()));
+        }
+        return b.build();
+    }
+    
+}
