31d7c5532137ba0f03f320b708b5d7a5891d5b27
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2014-12-02 18:05:47 +0100

    add QC support

--- MGX-DTOAdapter/src/main/java/de/cebitec/mgx/dtoadapter/QCResultDTOFactory.java
+++ MGX-DTOAdapter/src/main/java/de/cebitec/mgx/dtoadapter/QCResultDTOFactory.java
@@ -9,14 +9,16 @@ import de.cebitec.mgx.dto.dto.DataRowDTO;
 import de.cebitec.mgx.dto.dto.QCResultDTO;
 import de.cebitec.mgx.dto.dto.QCResultDTOList;
 import de.cebitec.mgx.qc.DataRow;
+import de.cebitec.mgx.qc.DataRowI;
 import de.cebitec.mgx.qc.QCResult;
+import de.cebitec.mgx.qc.QCResultI;
 import de.cebitec.mgx.util.AutoCloseableIterator;
 
 /**
  *
  * @author sj
  */
-public class QCResultDTOFactory extends DTOConversionBase<QCResult, QCResultDTO, QCResultDTOList> {
+public class QCResultDTOFactory extends DTOConversionBase<QCResultI, QCResultDTO, QCResultDTOList> {
 
     static {
         instance = new QCResultDTOFactory();
@@ -31,10 +33,10 @@ public class QCResultDTOFactory extends DTOConversionBase<QCResult, QCResultDTO,
     }
 
     @Override
-    public QCResultDTO toDTO(QCResult a) {
+    public QCResultDTO toDTO(QCResultI a) {
         QCResultDTO.Builder b = QCResultDTO.newBuilder();
         b.setName(a.getName());
-        for (DataRow dr : a.getData()) {
+        for (DataRowI dr : a.getData()) {
             DataRowDTO.Builder drdb = DataRowDTO.newBuilder();
             drdb.setName(dr.getName());
             for (float f : dr.getData()) {
@@ -51,7 +53,7 @@ public class QCResultDTOFactory extends DTOConversionBase<QCResult, QCResultDTO,
     }
 
     @Override
-    public QCResultDTOList toDTOList(AutoCloseableIterator<QCResult> list) {
+    public QCResultDTOList toDTOList(AutoCloseableIterator<QCResultI> list) {
         QCResultDTOList.Builder b = QCResultDTOList.newBuilder();
         while (list.hasNext()) {
             b = b.addResult(toDTO(list.next()));
