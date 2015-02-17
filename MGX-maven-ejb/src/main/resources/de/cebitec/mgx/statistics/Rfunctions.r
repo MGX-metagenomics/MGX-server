73c6a165c197d4a9a88bb262a54b1b987873343e
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-02-17 20:42:09 +0100

    suppress stupid startup messages from vegan package

--- MGX-maven-ejb/src/main/resources/de/cebitec/mgx/statistics/Rfunctions.r
+++ MGX-maven-ejb/src/main/resources/de/cebitec/mgx/statistics/Rfunctions.r
@@ -46,7 +46,7 @@ rareCompute <- function(x, rare.data, select) {
 rarefaction<-function(x, subsample=2, symbol=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) {
   library(lattice)
   library(permute)
-  library(vegan)
+  suppressPackageStartupMessages(library(vegan))
   library(parallel)
 
   x <- as.matrix(x)
