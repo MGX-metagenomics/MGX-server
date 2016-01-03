cd0c47d8d19ae30f39ae44e510eed593e09032cf
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-01-03 11:38:39 +0100

    minor changes

--- MGX-model/src/main/java/de/cebitec/mgx/model/misc/SequenceObservation.java
+++ MGX-model/src/main/java/de/cebitec/mgx/model/misc/SequenceObservation.java
@@ -5,40 +5,32 @@ package de.cebitec.mgx.model.misc;
  * @author sjaenick
  */
 public class SequenceObservation {
-    private int start;
-    private int stop;
-    private String attributeName;
-    private String attributeTypeName;
+
+    private final int start;
+    private final int stop;
+    private final String attributeName;
+    private final String attributeTypeName;
+
+    public SequenceObservation(int start, int stop, String attributeName, String attributeTypeName) {
+        this.start = start;
+        this.stop = stop;
+        this.attributeName = attributeName;
+        this.attributeTypeName = attributeTypeName;
+    }
 
     public int getStart() {
         return start;
-    }
-
-    public void setStart(int start) {
-        this.start = start;
     }
 
     public int getStop() {
         return stop;
     }
 
-    public void setStop(int stop) {
-        this.stop = stop;
-    }
-
     public String getAttributeName() {
         return attributeName;
     }
 
-    public void setAttributeName(String attributeName) {
-        this.attributeName = attributeName;
-    }
-
     public String getAttributeTypeName() {
         return attributeTypeName;
-    }
-
-    public void setAttributeTypeName(String attributeTypeName) {
-        this.attributeTypeName = attributeTypeName;
     }
 }
