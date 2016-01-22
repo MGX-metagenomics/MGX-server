ba0b195f22db0971c156e83bbec94e2bdfdbfca5
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-01-22 16:46:18 +0100

    minor optimization

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/UriProjectFilter.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/UriProjectFilter.java
@@ -3,10 +3,9 @@ package de.cebitec.mgx.web;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 import de.cebitec.gpms.core.MembershipI;
-import de.cebitec.gpms.core.ProjectClassI;
 import de.cebitec.gpms.core.UserI;
 import de.cebitec.gpms.data.DBGPMSI;
-import de.cebitec.gpms.data.DBMembershipI;
+import de.cebitec.gpms.data.JPAMasterI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
@@ -32,47 +31,33 @@ public class UriProjectFilter implements ContainerRequestFilter {
             Logger.getLogger(UriProjectFilter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    private static String getProjectFromPath(String path) {
-        int idx = path.indexOf('/');
-        if (idx != -1) {
-            return path.substring(0, idx);
-        } else {
-            return "";
-        }
-    }
-
-    private static String removeProjectFromPath(String path) {
-        int idx = path.indexOf('/');
-        if (idx != -1) {
-            return path.substring(idx + 1);
-        } else {
-            return path;
-        }
-    }
-
+    
     @Override
     public ContainerRequest filter(ContainerRequest cr) {
 
-        String project = getProjectFromPath(cr.getPath());
-        String resource = removeProjectFromPath(cr.getPath());
-
+        String path = cr.getPath(true);
+        String resource = path;
+        String projectName = "";
+        int idx = path.indexOf('/', 0);
+        if (idx != -1) {
+            projectName = path.substring(0, idx);
+            resource = path.substring(idx + 1);
+        }
+        
         UriBuilder ub = cr.getRequestUriBuilder();
         ub.replacePath(cr.getBaseUri().getPath() + resource);
         cr.setUris(cr.getBaseUri(), ub.build());
 
-        if ("GPMS".equals(project)) {
+        if ("GPMS".equals(projectName)) {
             return cr;
         }
 
         UserI u = gpms.getCurrentUser();
-        for (ProjectClassI pc : gpms.getSupportedProjectClasses()) {
-            for (MembershipI m : u.getMemberships(pc)) {
-                if (m.getProject().getName().equals(project)) {
-                    // will create a new master or re-use a cached one
-                    gpms.createMaster((DBMembershipI)m);
-                    return cr;
-                }
+        for (MembershipI m : u.getMemberships()) {
+            if (m.getProject().getName().equals(projectName)) {
+                // will create a new master or re-use a cached one
+                gpms.createMaster(m, JPAMasterI.class);
+                return cr;
             }
         }
         return cr;
