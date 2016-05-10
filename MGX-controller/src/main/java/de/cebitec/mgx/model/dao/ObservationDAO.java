d66e300625e62c9d4bfdec8649a78625680ddab6
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-05-10 18:14:25 +0200

    implement observation#create

--- MGX-controller/src/main/java/de/cebitec/mgx/model/dao/ObservationDAO.java
+++ MGX-controller/src/main/java/de/cebitec/mgx/model/dao/ObservationDAO.java
@@ -2,7 +2,9 @@ package de.cebitec.mgx.model.dao;
 
 import de.cebitec.mgx.controller.MGXController;
 import de.cebitec.mgx.core.MGXException;
+import de.cebitec.mgx.model.db.Attribute;
 import de.cebitec.mgx.model.db.Observation;
+import de.cebitec.mgx.model.db.Sequence;
 import de.cebitec.mgx.model.misc.SequenceObservation;
 import de.cebitec.mgx.util.DBIterator;
 import java.sql.Connection;
@@ -20,6 +22,20 @@ public class ObservationDAO<T extends Observation> {
 
     public ObservationDAO(MGXController ctx) {
         this.ctx = ctx;
+    }
+
+    public void create(SequenceObservation obs, Sequence seq, Attribute attr) throws MGXException {
+        try (Connection conn = ctx.getConnection()) {
+            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO observation (start, stop, seq_id, attr_id) VALUES (?,?,?,?)")) {
+                stmt.setInt(1, obs.getStart());
+                stmt.setInt(2, obs.getStop());
+                stmt.setLong(3, seq.getId());
+                stmt.setLong(4, attr.getId());
+                stmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new MGXException(ex.getMessage());
+        }
     }
 
     public DBIterator<SequenceObservation> byRead(long seqId) throws MGXException {
