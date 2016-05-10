1fd94c1fe24e130bfc0c44f818d1720661cd9adb
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-05-10 18:14:25 +0200

    implement observation#create

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/ObservationBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/ObservationBean.java
@@ -1,20 +1,28 @@
 package de.cebitec.mgx.web;
 
+import de.cebitec.gpms.security.Secure;
 import de.cebitec.mgx.controller.MGX;
 import de.cebitec.mgx.controller.MGXController;
+import de.cebitec.mgx.controller.MGXRoles;
 import de.cebitec.mgx.core.MGXException;
+import de.cebitec.mgx.dto.dto.ObservationDTO;
 import de.cebitec.mgx.dto.dto.ObservationDTOList;
 import de.cebitec.mgx.dtoadapter.ObservationDTOFactory;
+import de.cebitec.mgx.model.db.Attribute;
+import de.cebitec.mgx.model.db.Sequence;
 import de.cebitec.mgx.model.misc.SequenceObservation;
 import de.cebitec.mgx.util.DBIterator;
 import de.cebitec.mgx.web.exception.MGXWebException;
 import de.cebitec.mgx.web.helper.ExceptionMessageConverter;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -27,6 +35,23 @@ public class ObservationBean {
     @Inject
     @MGX
     MGXController mgx;
+
+    @PUT
+    @Path("create/{seqId}/{attrId}")
+    @Consumes("application/x-protobuf")
+    @Produces("application/x-protobuf")
+    @Secure(rightsNeeded = {MGXRoles.User, MGXRoles.Admin})
+    public Response create(@PathParam("seqId") Long seqId, @PathParam("attrId") Long attrId, ObservationDTO dto) {
+        try {
+            Sequence seq = mgx.getSequenceDAO().getById(seqId);
+            Attribute attr = mgx.getAttributeDAO().getById(attrId);
+            SequenceObservation obs = ObservationDTOFactory.getInstance().toDB(dto);
+            mgx.getObservationDAO().create(obs, seq, attr);
+        } catch (MGXException ex) {
+            throw new MGXWebException(ExceptionMessageConverter.convert(ex.getMessage()));
+        }
+        return Response.ok().build();
+    }
 
     @GET
     @Path("byRead/{id}")
