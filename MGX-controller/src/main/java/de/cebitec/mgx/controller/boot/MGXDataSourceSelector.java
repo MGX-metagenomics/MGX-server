724b00cc54025f26d5268985db85399f00315a25
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-05-20 19:56:09 +0200

    fix datasource selection for BI setup (which only has o2dbi2)

--- MGX-controller/src/main/java/de/cebitec/mgx/controller/boot/MGXDataSourceSelector.java
+++ MGX-controller/src/main/java/de/cebitec/mgx/controller/boot/MGXDataSourceSelector.java
@@ -1,4 +1,3 @@
-
 package de.cebitec.mgx.controller.boot;
 
 import de.cebitec.gpms.core.DataSourceI;
@@ -18,18 +17,25 @@ public class MGXDataSourceSelector extends GPMSDataSourceSelector {
 //    private final static DBAPITypeI MGX_DBAPI_TYPE = new DBAPIType("MGX");
 //    private final static DBAPITypeI REST_DBAPI_TYPE = new DBAPIType("REST");
 //    private final static DataSourceTypeI MGX_DS_TYPE = new DataSourceType("MGX");
-
     @Override
     public <T extends MasterI> DataSourceI selectFromDataSources(MembershipI mbr, Class<T> masterClass) throws GPMSException {
-        
+
         if (!JPAMasterI.class.isAssignableFrom(masterClass)) {
             throw new GPMSException("MGX requires JPA as a master class.");
+        }
+        if (mbr.getProject().getDataSources().isEmpty()) {
+            throw new GPMSException(mbr.getProject().getName() + " has no Datasources.");
         }
         for (DataSourceI gpmsDS : mbr.getProject().getDataSources()) {
             if (gpmsDS instanceof DataSource_DBI) {
                 DataSource_DBI dsdb = (DataSource_DBI) gpmsDS;
                 if ("MGX".equals(dsdb.getType().getName()) && "MGX".equals(dsdb.getAPIType().getName())) {
                     return dsdb;
+                } else if ("MGX".equals(dsdb.getType().getName()) && "O2DBI2".equals(dsdb.getAPIType().getName())) {
+                    // Bielefeld setup does not have a MGX api type yet
+                    return dsdb;
+                } else {
+                    System.err.println("Skipping datasource type "+dsdb.getType().getName()+ " with API type "+dsdb.getAPIType().getName());
                 }
             }
         }
