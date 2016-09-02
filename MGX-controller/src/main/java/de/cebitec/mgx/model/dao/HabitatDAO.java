59a2541f705e1d39ffecc696098a7561034c22cd
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-09-02 15:10:49 +0200

    habitat#fetchall converted to native SQL

--- MGX-controller/src/main/java/de/cebitec/mgx/model/dao/HabitatDAO.java
+++ MGX-controller/src/main/java/de/cebitec/mgx/model/dao/HabitatDAO.java
@@ -4,6 +4,8 @@ import de.cebitec.mgx.controller.MGXControllerImpl;
 import de.cebitec.mgx.core.MGXException;
 import de.cebitec.mgx.model.db.Habitat;
 import de.cebitec.mgx.model.db.Identifiable;
+import de.cebitec.mgx.util.AutoCloseableIterator;
+import de.cebitec.mgx.util.DBIterator;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,7 +26,8 @@ public class HabitatDAO<T extends Habitat> extends DAO<T> {
         return Habitat.class;
     }
 
-    private static final String GET_HABITAT = "SELECT name, altitude, biome, description, latitude, longitude FROM habitat WHERE id=?";
+    private static final String GET_HABITATS = "SELECT id, name, altitude, biome, description, latitude, longitude FROM habitat";
+    private static final String GET_HABITAT_BY_ID = "SELECT name, altitude, biome, description, latitude, longitude FROM habitat WHERE id=?";
 
     @Override
     public <T extends Identifiable> T getById(Long id) throws MGXException {
@@ -33,7 +36,7 @@ public class HabitatDAO<T extends Habitat> extends DAO<T> {
         }
 
         try (Connection conn = getConnection()) {
-            try (PreparedStatement stmt = conn.prepareStatement(GET_HABITAT)) {
+            try (PreparedStatement stmt = conn.prepareStatement(GET_HABITAT_BY_ID)) {
                 stmt.setLong(1, id);
                 try (ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
@@ -54,6 +57,32 @@ public class HabitatDAO<T extends Habitat> extends DAO<T> {
         }
 
         throw new MGXException("No object of type " + getClassName() + " for ID " + id + ".");
+    }
+
+    @Override
+    public AutoCloseableIterator<T> getAll() throws MGXException {
+        try {
+            Connection conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(GET_HABITATS);
+            ResultSet rs = stmt.executeQuery();
+
+            return new DBIterator<T>(rs, stmt, conn) {
+                @Override
+                public T convert(ResultSet rs) throws SQLException {
+                    Habitat ret = new Habitat();
+                    ret.setId(rs.getLong(1));
+                    ret.setName(rs.getString(2));
+                    ret.setAltitude(rs.getInt(3));
+                    ret.setBiome(rs.getString(4));
+                    ret.setDescription(rs.getString(5));
+                    ret.setLatitude(rs.getDouble(6));
+                    ret.setLongitude(rs.getDouble(7));
+                    return (T) ret;
+                }
+            };
+        } catch (SQLException ex) {
+            throw new MGXException(ex);
+        }
     }
 
 }
