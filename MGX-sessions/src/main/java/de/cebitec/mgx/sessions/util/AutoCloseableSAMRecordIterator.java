fdf02f34977a01d9deecfb914a4e384350a963bf
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-02-03 16:36:14 +0100

    remove check for discard flag from mappings

--- MGX-sessions/src/main/java/de/cebitec/mgx/sessions/util/AutoCloseableSAMRecordIterator.java
+++ MGX-sessions/src/main/java/de/cebitec/mgx/sessions/util/AutoCloseableSAMRecordIterator.java
@@ -7,14 +7,8 @@ import htsjdk.samtools.CigarElement;
 import htsjdk.samtools.CigarOperator;
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMRecordIterator;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.locks.Lock;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -23,13 +17,11 @@ import java.util.logging.Logger;
 public class AutoCloseableSAMRecordIterator implements AutoCloseableIterator<MappedSequence> {
 
     private final SAMRecordIterator iterator;
-    private final Connection conn;
     private final Lock lock;
     private MappedSequence curElem = null;
 
-    public AutoCloseableSAMRecordIterator(SAMRecordIterator iterator, Connection conn, Lock l) {
+    public AutoCloseableSAMRecordIterator(SAMRecordIterator iterator, Lock l) {
         this.iterator = iterator;
-        this.conn = conn;
         this.lock = l;
     }
 
@@ -54,40 +46,22 @@ public class AutoCloseableSAMRecordIterator implements AutoCloseableIterator<Map
 
             SAMRecord record = iterator.next();
 
-            // sequence might have been marked discard after the mapping was created,
-            // need to check and filter
-            boolean discard = false;
-            long seqId = Long.parseLong(record.getReadName());
-
-            try (PreparedStatement stmt = conn.prepareStatement("SELECT discard FROM read WHERE id=?")) {
-                stmt.setLong(1, seqId);
-                try (ResultSet rs = stmt.executeQuery()) {
-                    while (rs.next()) {
-                        discard = rs.getBoolean(1);
-                    }
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(getClass().getName()).log(Level.INFO, null, ex);
-            }
-
-            if (!discard) {
-                // convert to 0-based positions
-                if (record.getReadNegativeStrandFlag()) {
-                    curElem = new MappedSequence(Long.parseLong(record.getReadName()),
-                            record.getAlignmentEnd() - 1,
-                            record.getAlignmentStart() - 1,
-                            getIdentity(record.getCigar()));
-                } else {
-                    curElem = new MappedSequence(Long.parseLong(record.getReadName()),
-                            record.getAlignmentStart() - 1,
-                            record.getAlignmentEnd() - 1,
-                            getIdentity(record.getCigar()));
-                }
+            // convert to 0-based positions
+            if (record.getReadNegativeStrandFlag()) {
+                curElem = new MappedSequence(Long.parseLong(record.getReadName()),
+                        record.getAlignmentEnd() - 1,
+                        record.getAlignmentStart() - 1,
+                        getIdentity(record.getCigar()));
+            } else {
+                curElem = new MappedSequence(Long.parseLong(record.getReadName()),
+                        record.getAlignmentStart() - 1,
+                        record.getAlignmentEnd() - 1,
+                        getIdentity(record.getCigar()));
             }
         }
     }
 
-    private float getIdentity(Cigar c) {
+    private static float getIdentity(Cigar c) {
         int matched = 0;
         int alnLen = 0;
         for (CigarElement elem : c.getCigarElements()) {
@@ -109,11 +83,6 @@ public class AutoCloseableSAMRecordIterator implements AutoCloseableIterator<Map
     @Override
     public void close() {
         iterator.close();
-        try {
-            conn.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(AutoCloseableSAMRecordIterator.class.getName()).log(Level.SEVERE, null, ex);
-        }
         lock.unlock();
     }
 }
