e46e762808935d9efdebb168c9e9e0f4d015f5d6
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-03-09 19:07:44 +0100

    convert to subscription-based datasources

--- MGX-upload/src/main/java/de/cebitec/mgx/download/SeqRunDownloadProvider.java
+++ MGX-upload/src/main/java/de/cebitec/mgx/download/SeqRunDownloadProvider.java
@@ -1,7 +1,8 @@
 package de.cebitec.mgx.download;
 
 import com.google.protobuf.ByteString;
-import de.cebitec.mgx.configuration.api.MGXConfigurationI;
+import de.cebitec.gpms.util.GPMSManagedConnectionI;
+import de.cebitec.gpms.util.GPMSManagedDataSourceI;
 import de.cebitec.mgx.core.MGXException;
 import de.cebitec.mgx.dto.dto.SequenceDTO;
 import de.cebitec.mgx.dto.dto.SequenceDTOList;
@@ -11,19 +12,19 @@ import de.cebitec.mgx.sequence.DNASequenceI;
 import de.cebitec.mgx.sequence.SeqReaderFactory;
 import de.cebitec.mgx.sequence.SeqReaderI;
 import de.cebitec.mgx.sequence.SeqStoreException;
-import java.io.File;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.EJB;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 /**
  *
@@ -32,39 +33,28 @@ import javax.sql.DataSource;
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList> {
 
-    @EJB
-    MGXConfigurationI mgxconfig;
     protected final String projectName;
-    protected final DataSource dataSource;
+    private final GPMSManagedDataSourceI dataSource;
     protected SeqReaderI<? extends DNASequenceI> reader;
     protected long lastAccessed;
-    protected int bulksize;
+    protected int maxSeqsPerChunk = 200;
 
-    public SeqRunDownloadProvider(DataSource dataSource, String projName, long run_id) throws MGXException {
-        this(dataSource, projName);
-        File file = getStorageFile(run_id);
+    public SeqRunDownloadProvider(GPMSManagedDataSourceI dataSource, String projName, String dbFile) throws MGXException {
+        this.projectName = projName;
+        this.dataSource = dataSource;
+        this.dataSource.subscribe();
+
         try {
-            reader = SeqReaderFactory.<DNASequenceI>getReader(file.getAbsolutePath());
+            reader = SeqReaderFactory.<DNASequenceI>getReader(dbFile);
         } catch (SeqStoreException ex) {
             throw new MGXException("Could not initialize sequence download: " + ex.getMessage());
         }
+
+        lastAccessed = System.currentTimeMillis();
     }
 
-    protected SeqRunDownloadProvider(DataSource dataSource, String projName) throws MGXException {
-        projectName = projName;
-
-        try {
-            assert mgxconfig == null;
-            mgxconfig = InitialContext.<MGXConfigurationI>doLookup("java:global/MGX-maven-ear/MGX-configuration/MGXConfiguration!de.cebitec.mgx.configuration.api.MGXConfigurationI");
-
-            this.dataSource = dataSource;
-            //conn.setClientInfo("ApplicationName", "MGX-SeqDownload (" + projName + ")");
-        } catch (NamingException ex) {
-            throw new MGXException("Could not initialize sequence download: " + ex.getMessage());
-        }
-
-        bulksize = mgxconfig.getSQLBulkInsertSize();
-        lastAccessed = System.currentTimeMillis();
+    protected GPMSManagedConnectionI getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
     @Override
@@ -74,8 +64,6 @@ public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList
                 reader.close();
                 reader = null;
             }
-//            conn.setClientInfo("ApplicationName", "");
-//            conn.close();
         } catch (SeqStoreException ex) {
             Logger.getLogger(SeqRunDownloadProvider.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -83,13 +71,13 @@ public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList
 
     @Override
     public void close() throws MGXException {
+        dataSource.close();
+
         try {
             if (reader != null) {
                 reader.close();
                 reader = null;
             }
-//            conn.setClientInfo("ApplicationName", "");
-//            conn.close();
         } catch (SeqStoreException ex) {
             Logger.getLogger(SeqRunDownloadProvider.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -97,43 +85,54 @@ public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList
 
     @Override
     public SequenceDTOList fetch() throws MGXException {
-        Builder listBuilder = SequenceDTOList.newBuilder();
         int count = 0;
         try {
-            while (count <= bulksize && reader.hasMoreElements()) {
+            //
+            // fetch sequences
+            //
+            List<DNASequenceI> seqs = new LinkedList<>();
+            while (count <= maxSeqsPerChunk && reader.hasMoreElements()) {
                 DNASequenceI seq = reader.nextElement();
-                String seqName = getSequenceName(seq.getId());
-                if (seqName != null) {
-                    SequenceDTO.Builder dtob = SequenceDTO.newBuilder()
-                            .setId(seq.getId())
-                            .setName(seqName)
-                            .setSequence(new String(seq.getSequence()));
+                seqs.add(seq);
+                count++;
+            }
 
-                    if (seq instanceof DNAQualitySequenceI) {
-                        DNAQualitySequenceI qseq = (DNAQualitySequenceI) seq;
-                        dtob.setQuality(ByteString.copyFrom(qseq.getQuality()));
+            //
+            // get names from database
+            //
+            if (!seqs.isEmpty()) {
+                getSequenceNames(seqs);
+
+                // additional check
+                for (DNASequenceI seq : seqs) {
+                    if (seq.getName() == null) {
+                        throw new MGXException("No name for read id " + seq.getId() + " in project " + projectName);
                     }
-                    listBuilder.addSeq(dtob.build());
-                    count++;
                 }
+            }
+            //
+            // convert to DTO
+            //
+            Builder listBuilder = SequenceDTOList.newBuilder();
+            for (DNASequenceI seq : seqs) {
+                SequenceDTO.Builder dtob = SequenceDTO.newBuilder()
+                        .setId(seq.getId())
+                        .setNameBytes(ByteString.copyFrom(seq.getName()))
+                        .setSequenceBytes(ByteString.copyFrom(seq.getSequence()));
+
+                if (seq instanceof DNAQualitySequenceI) {
+                    dtob.setQuality(ByteString.copyFrom(((DNAQualitySequenceI) seq).getQuality()));
+                }
+                listBuilder.addSeq(dtob.build());
             }
             lastAccessed = System.currentTimeMillis();
             listBuilder.setComplete(!reader.hasMoreElements());
             return listBuilder.build();
+
         } catch (SeqStoreException ex) {
             throw new MGXException(ex);
         }
-    }
 
-    @Override
-    public boolean isFinished() {
-        lastAccessed = System.currentTimeMillis();
-        try {
-            return !reader.hasMoreElements();
-        } catch (SeqStoreException ex) {
-            Logger.getLogger(SeqRunDownloadProvider.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return true;
     }
 
     @Override
@@ -147,28 +146,34 @@ public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList
         return lastAccessed;
     }
 
-    private File getStorageFile(long run_id) throws MGXException {
-        StringBuilder fname = new StringBuilder(mgxconfig.getPersistentDirectory().getAbsolutePath())
-                .append(File.separator).append(getProjectName())
-                .append(File.separator).append("seqruns")
-                .append(File.separator).append(run_id);
+    private void getSequenceNames(List<DNASequenceI> seqs) throws MGXException {
+        //
+        // sort by id, ascending
+        //
+        Collections.sort(seqs, new Comparator<DNASequenceI>() {
+            @Override
+            public int compare(DNASequenceI o1, DNASequenceI o2) {
+                return Long.compare(o1.getId(), o2.getId());
+            }
 
-        File ret = new File(fname.toString());
-        if (!ret.exists()) {
-            throw new MGXException("Missing storage file for run " + run_id);
-        }
-        return ret;
-    }
-
-    private String getSequenceName(long seqId) throws MGXException {
+        });
+        
         try (Connection conn = dataSource.getConnection()) {
-            try (PreparedStatement stmt = conn.prepareStatement("SELECT name from read WHERE id=?")) {
-                stmt.setLong(1, seqId);
+            try (PreparedStatement stmt = conn.prepareStatement(buildSQL(seqs.size()))) {
+                int idx = 1;
+                for (DNASequenceI seq : seqs) {
+                    stmt.setLong(idx++, seq.getId());
+                }
+
+                Iterator<DNASequenceI> seqIter = seqs.iterator();
+
                 try (ResultSet rs = stmt.executeQuery()) {
-                    if (rs.next()) {
-                        return rs.getString(1);
-                    } else {
-                        throw new MGXException("No name for read id " + seqId + " in project " + projectName);
+                    while (rs.next() && seqIter.hasNext()) {
+                        long seqId = rs.getLong(1);
+                        DNASequenceI seq = seqIter.next();
+                        
+                        //assert seqId == seq.getId();
+                        seq.setName(rs.getString(2).getBytes());
                     }
                 }
             }
@@ -176,5 +181,16 @@ public class SeqRunDownloadProvider implements DownloadProviderI<SequenceDTOList
             Logger.getLogger(SeqRunDownloadProvider.class.getName()).log(Level.SEVERE, null, ex);
             throw new MGXException(ex);
         }
+
+    }
+
+    private static String buildSQL(int numElements) {
+        assert numElements > 0;
+        StringBuilder sb = new StringBuilder("SELECT id, name FROM read WHERE id IN (?");
+        for (int i = 1; i < numElements; i++) {
+            sb.append(",?");
+        }
+        sb.append(") ORDER BY id ASC");
+        return sb.toString();
     }
 }
--- MGX-upload/src/main/java/de/cebitec/mgx/upload/ReferenceUploadReceiver.java
+++ MGX-upload/src/main/java/de/cebitec/mgx/upload/ReferenceUploadReceiver.java
@@ -1,10 +1,12 @@
 package de.cebitec.mgx.upload;
 
+import de.cebitec.gpms.util.GPMSManagedDataSourceI;
 import de.cebitec.mgx.core.MGXException;
 import de.cebitec.mgx.dto.dto.RegionDTO;
 import de.cebitec.mgx.dto.dto.RegionDTOList;
 import de.cebitec.mgx.model.db.Reference;
 import de.cebitec.mgx.model.db.Region;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -31,29 +33,33 @@ public class ReferenceUploadReceiver implements UploadReceiverI<RegionDTOList> {
     private final String refName;
     //
     private final String projectName;
-    private final Connection conn;
+    private GPMSManagedDataSourceI dataSource;
     //
     private long lastAccessed;
     private File fasta = null;
-    private FileWriter writer = null;
+    private BufferedWriter writer = null;
     private int dnaSize = 0;
     private List<Region> regions;
 
-    public ReferenceUploadReceiver(Reference reference, String projectName, Connection conn) {
+    public ReferenceUploadReceiver(Reference reference, String projectName, GPMSManagedDataSourceI dataSource) {
         this.reference = reference;
         this.refFile = reference.getFile();
         this.referenceId = reference.getId();
         this.refName = reference.getName();
         this.projectName = projectName;
-        this.conn = conn;
+        this.dataSource = dataSource;
+
+        dataSource.subscribe();
         lastAccessed = System.currentTimeMillis();
     }
 
     public void addSequenceData(String dna) throws IOException {
         if (fasta == null) {
             fasta = new File(refFile);
-            writer = new FileWriter(fasta);
-            writer.append(">" + refName + "\n");
+            writer = new BufferedWriter(new FileWriter(fasta));
+            writer.append(">");
+            writer.append(refName);
+            writer.newLine();
         }
         writer.append(dna.toUpperCase());
         dnaSize += dna.length();
@@ -73,12 +79,9 @@ public class ReferenceUploadReceiver implements UploadReceiverI<RegionDTOList> {
         if (fasta != null && fasta.exists()) {
             fasta.delete();
         }
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(ReferenceUploadReceiver.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        if (dataSource != null) {
+            dataSource.close();
+            dataSource = null;
         }
     }
 
@@ -89,7 +92,7 @@ public class ReferenceUploadReceiver implements UploadReceiverI<RegionDTOList> {
         }
         if (writer != null) {
             try {
-                writer.append(System.lineSeparator());
+                writer.newLine();
                 writer.close();
                 writer = null;
             } catch (IOException ex) {
@@ -98,33 +101,42 @@ public class ReferenceUploadReceiver implements UploadReceiverI<RegionDTOList> {
         }
         //reference.setRegions(regions);
         reference.setFile(fasta.getAbsolutePath());
-        try (PreparedStatement stmt = conn.prepareStatement("UPDATE reference SET ref_filepath=? WHERE id=?")) {
-            stmt.setString(1, refFile);
-            stmt.setLong(2, referenceId);
-            stmt.execute();
+
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement stmt = conn.prepareStatement("UPDATE reference SET ref_filepath=? WHERE id=?")) {
+                stmt.setString(1, refFile);
+                stmt.setLong(2, referenceId);
+                stmt.execute();
+            }
         } catch (SQLException ex) {
             cancel();
             throw new MGXException(ex.getMessage());
         }
 
         if (regions != null && !regions.isEmpty()) {
-            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO region (name, description, type, reg_start, reg_stop, ref_id) VALUES (?,?,?,?,?,?)")) {
-                for (Region r : regions) {
-                    stmt.setString(1, r.getName());
-                    stmt.setString(2, r.getDescription());
-                    stmt.setString(3, r.getType());
-                    stmt.setInt(4, r.getStart());
-                    stmt.setInt(5, r.getStop());
-                    stmt.setLong(6, referenceId);
-                    stmt.addBatch();
+            try (Connection conn = dataSource.getConnection()) {
+                try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO region (name, description, type, reg_start, reg_stop, ref_id) VALUES (?,?,?,?,?,?)")) {
+                    for (Region r : regions) {
+                        stmt.setString(1, r.getName());
+                        stmt.setString(2, r.getDescription());
+                        stmt.setString(3, r.getType());
+                        stmt.setInt(4, r.getStart());
+                        stmt.setInt(5, r.getStop());
+                        stmt.setLong(6, referenceId);
+                        stmt.addBatch();
+                    }
+                    stmt.executeBatch();
                 }
-                stmt.executeBatch();
-                conn.close();
             } catch (SQLException ex) {
                 Logger.getLogger(ReferenceUploadReceiver.class.getName()).log(Level.SEVERE, null, ex);
                 cancel();
                 throw new MGXException(ex.getMessage());
             }
+        }
+
+        if (dataSource != null) {
+            dataSource.close();
+            dataSource = null;
         }
     }
 
