db15988f6239be528378344d9a7408cb46e968ae
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2019-12-20 14:51:54 +0100

    implement news messages

--- MGX-maven-web/src/main/java/de/cebitec/mgx/web/ProjectBean.java
+++ MGX-maven-web/src/main/java/de/cebitec/mgx/web/ProjectBean.java
@@ -1,5 +1,6 @@
 package de.cebitec.mgx.web;
 
+import de.cebitec.gpms.core.GPMSMessageI;
 import de.cebitec.gpms.core.MembershipI;
 import de.cebitec.gpms.core.ProjectClassI;
 import de.cebitec.gpms.data.DBGPMSI;
@@ -7,12 +8,15 @@ import de.cebitec.gpms.dto.ProjectClassDTOFactory;
 import de.cebitec.gpms.dto.ProjectDTOFactory;
 import de.cebitec.gpms.dto.RoleDTOFactory;
 import de.cebitec.gpms.dto.impl.GPMSLong;
+import de.cebitec.gpms.dto.impl.GPMSMessageDTO;
+import de.cebitec.gpms.dto.impl.GPMSMessageDTOList;
 import de.cebitec.gpms.dto.impl.GPMSString;
 import de.cebitec.gpms.dto.impl.MembershipDTO;
 import de.cebitec.gpms.dto.impl.MembershipDTOList;
 import de.cebitec.gpms.dto.impl.ProjectClassDTOList;
 import de.cebitec.mgx.util.ForwardingIterator;
 import java.util.Collection;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
@@ -52,13 +56,26 @@ public class ProjectBean {
     }
 
     @GET
+    @Path("getMessages")
+    @Produces("application/x-protobuf")
+    public GPMSMessageDTOList getMessages() {
+        GPMSMessageDTOList.Builder ret = GPMSMessageDTOList.newBuilder();
+        List<GPMSMessageI> messages = gpms.getMessages();
+        for (GPMSMessageI msg : messages) {
+            GPMSMessageDTO.Builder b = GPMSMessageDTO.newBuilder();
+            b.setDate(msg.getDate().getTime() / 1000L);
+            b.setMessage(msg.getText());
+            ret.addMessage(b.build());
+        }
+        return ret.build();
+    }
+
+    @GET
     @Path("listMemberships")
     @Produces("application/x-protobuf")
     public MembershipDTOList listMemberships() {
         MembershipDTOList.Builder ret = MembershipDTOList.newBuilder();
 
-//        for (ProjectClassI pc : gpms.getSupportedProjectClasses()) {
-//            if ("MGX".equals(pc.getName())) {
         Collection<MembershipI> memberships = gpms.getCurrentUser().getMemberships();
         for (MembershipI m : memberships) {
             MembershipDTO dto = MembershipDTO.newBuilder()
@@ -66,8 +83,6 @@ public class ProjectBean {
                     .setRole(RoleDTOFactory.getInstance().toDTO(m.getRole()))
                     .build();
             ret.addMembership(dto);
-//                }
-//            }
         }
         return ret.build();
     }
