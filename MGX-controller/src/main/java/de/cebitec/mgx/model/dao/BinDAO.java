a6d381726a43e416e7e57fd9940722a6918c2637
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-07-04 11:42:36 +0200

    add logging

--- MGX-controller/src/main/java/de/cebitec/mgx/model/dao/BinDAO.java
+++ MGX-controller/src/main/java/de/cebitec/mgx/model/dao/BinDAO.java
@@ -24,19 +24,19 @@ import java.util.logging.Logger;
  * @author sjaenick
  */
 public class BinDAO extends DAO<Bin> {
-
+    
     public BinDAO(MGXController ctx) {
         super(ctx);
     }
-
+    
     @Override
     Class<Bin> getType() {
         return Bin.class;
     }
-
+    
     private final static String CREATE = "INSERT INTO bin (name, completeness, contamination, taxonomy, n50, assembly_id) "
             + "VALUES (?,?,?,?,?,?) RETURNING id";
-
+    
     @Override
     public long create(Bin obj) throws MGXException {
 
@@ -46,7 +46,7 @@ public class BinDAO extends DAO<Bin> {
         if (obj.getName().toLowerCase().equals("unclassified") || obj.getName().toLowerCase().equals("unbinned")) {
             obj.setTaxonomy("root");
         }
-
+        
         try ( Connection conn = getConnection()) {
             try ( PreparedStatement stmt = conn.prepareStatement(CREATE)) {
                 stmt.setString(1, obj.getName());
@@ -55,7 +55,7 @@ public class BinDAO extends DAO<Bin> {
                 stmt.setString(4, obj.getTaxonomy());
                 stmt.setLong(5, obj.getN50());
                 stmt.setLong(6, obj.getAssemblyId());
-
+                
                 try ( ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
                         obj.setId(rs.getLong(1));
@@ -67,9 +67,9 @@ public class BinDAO extends DAO<Bin> {
         }
         return obj.getId();
     }
-
+    
     private final static String UPDATE = "UPDATE bin SET name=?, completeness=?, contamination=?, taxonomy=?, n50=?, predicted_cds=?, total_bp=?, assembly_id=? WHERE id=?";
-
+    
     public void update(Bin obj) throws MGXException {
         if (obj.getId() == Bin.INVALID_IDENTIFIER) {
             throw new MGXException("Cannot update object of type Bin without an ID.");
@@ -84,7 +84,7 @@ public class BinDAO extends DAO<Bin> {
                 stmt.setInt(6, obj.getPredictedCDS());
                 stmt.setLong(7, obj.getTotalBp());
                 stmt.setLong(8, obj.getAssemblyId());
-
+                
                 stmt.setLong(9, obj.getId());
                 int numRows = stmt.executeUpdate();
                 if (numRows != 1) {
@@ -95,15 +95,15 @@ public class BinDAO extends DAO<Bin> {
             throw new MGXException(ex);
         }
     }
-
+    
     private static final String BY_ID = "SELECT id, name, completeness, contamination, taxonomy, n50, predicted_cds, total_bp, assembly_id FROM bin WHERE id=?";
-
+    
     @Override
     public Result<Bin> getById(long id) {
         if (id <= 0) {
             return Result.error("No/Invalid ID supplied.");
         }
-
+        
         try ( Connection conn = getConnection()) {
             try ( PreparedStatement stmt = conn.prepareStatement(BY_ID)) {
                 stmt.setLong(1, id);
@@ -127,19 +127,19 @@ public class BinDAO extends DAO<Bin> {
             getController().log(ex);
             return Result.error(ex.getMessage());
         }
-
+        
         return Result.error("No object of type Bin for ID " + id + ".");
     }
-
+    
     private static final String FETCHALL = "SELECT id, name, completeness, contamination, taxonomy, n50, predicted_cds, total_bp, assembly_id FROM bin";
-
+    
     public Result<AutoCloseableIterator<Bin>> getAll() {
-
+        
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement(FETCHALL);
             ResultSet rs = stmt.executeQuery();
-
+            
             DBIterator<Bin> dbIterator = new DBIterator<Bin>(rs, stmt, conn) {
                 @Override
                 public Bin convert(ResultSet rs) throws SQLException {
@@ -162,17 +162,17 @@ public class BinDAO extends DAO<Bin> {
             return Result.error(ex.getMessage());
         }
     }
-
+    
     private static final String BY_ASM = "SELECT b.id, b.name, b.completeness, b.contamination, b.taxonomy, b.n50, b.num_contigs, b.total_bp, b.predicted_cds FROM bin b WHERE b.assembly_id=?";
-
+    
     public Result<AutoCloseableIterator<Bin>> byAssembly(final long asm_id) {
-
+        
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement(BY_ASM);
             stmt.setLong(1, asm_id);
             ResultSet rs = stmt.executeQuery();
-
+            
             DBIterator<Bin> dbIterator = new DBIterator<Bin>(rs, stmt, conn) {
                 @Override
                 public Bin convert(ResultSet rs) throws SQLException {
@@ -191,19 +191,19 @@ public class BinDAO extends DAO<Bin> {
                 }
             };
             return Result.ok(dbIterator);
-
+            
         } catch (SQLException ex) {
             return Result.error(ex.getMessage());
         }
     }
-
+    
     public Result<TaskI> delete(long bin_id) {
         Result<Bin> res = getById(bin_id);
         if (res.isError()) {
             return Result.error(res.getError());
         }
         Bin bin = res.getValue();
-
+        
         try {
             File assemblyDir = new File(getController().getProjectAssemblyDirectory(), String.valueOf(bin.getAssemblyId()));
             File binFasta = new File(assemblyDir, String.valueOf(bin_id) + ".fna");
@@ -214,11 +214,11 @@ public class BinDAO extends DAO<Bin> {
             getController().log(ex);
             return Result.error(ex.getMessage());
         }
-
+        
         TaskI t = new DeleteBin(getController().getDataSource(), bin_id, getController().getProjectName(), new TaskI[]{});
         return Result.ok(t);
     }
-
+    
     private static final String BINS_BY_ASM_TMPL = "WITH temp as ( "
             + "SELECT b.id, b.name, b.completeness, b.contamination, b.taxonomy, b.n50, count(c.id), sum(c.length_bp) FROM assembly a "
             + "LEFT JOIN bin b ON (a.id=b.assembly_id) "
@@ -232,7 +232,7 @@ public class BinDAO extends DAO<Bin> {
             + "LEFT JOIN gene g ON (c.id=g.contig_id) "
             + "GROUP BY t.id, t.name, t.completeness, t.contamination, t.taxonomy, t.n50, t.count, t.sum "
             + "ORDER BY t.id ASC";
-
+    
     public void updateDerivedFields(long asm_id) throws MGXException {
         try ( Connection conn = getConnection()) {
             try ( PreparedStatement stmt = conn.prepareStatement(BINS_BY_ASM_TMPL)) {
@@ -258,7 +258,7 @@ public class BinDAO extends DAO<Bin> {
                             stmt2.setLong(4, ret.getId());
                             stmt2.executeUpdate();
                         }
-
+                        
                         indexFASTA(asm_id, ret.getId());
                     }
                 }
@@ -268,7 +268,7 @@ public class BinDAO extends DAO<Bin> {
             throw new MGXException(ex);
         }
     }
-
+    
     public void indexFASTA(File binFasta) throws MGXException {
         File dir = binFasta.getParentFile();
         if (!dir.exists() && dir.canWrite()) {
@@ -276,19 +276,20 @@ public class BinDAO extends DAO<Bin> {
             throw new MGXException("Cannot write to directory " + dir.getAbsolutePath());
         }
         try {
+            getController().log("Creating FASTA index for " + binFasta.getAbsolutePath());
             FastaSequenceIndexCreator.create(binFasta.toPath(), true);
         } catch (IOException ex) {
             Logger.getLogger(BinDAO.class.getName()).log(Level.SEVERE, null, ex);
             throw new MGXException(ex);
         }
         File indexFile = new File(binFasta.getAbsolutePath() + ".fai");
-
+        
         if (!indexFile.exists()) {
             Logger.getLogger(BinDAO.class.getName()).log(Level.SEVERE, "Index creation failed for {0}", binFasta.getAbsolutePath());
             throw new MGXException("Index creation failed.");
         }
     }
-
+    
     private void indexFASTA(long asm_id, long bin_id) throws MGXException {
         try {
             File assemblyDir = new File(getController().getProjectAssemblyDirectory(), String.valueOf(asm_id));
