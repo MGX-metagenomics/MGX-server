a8c8273f49dc6d653a5fdf362fb6188cfc89289e
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-07-04 14:57:38 +0200

    implement timeout with cleanup to avoid unclosed DB connections

--- MGX-sessions/src/main/java/de/cebitec/mgx/sessions/IteratorHolder.java
+++ MGX-sessions/src/main/java/de/cebitec/mgx/sessions/IteratorHolder.java
@@ -1,13 +1,20 @@
 package de.cebitec.mgx.sessions;
 
+import de.cebitec.mgx.core.TaskI;
 import de.cebitec.mgx.util.AutoCloseableIterator;
 import jakarta.ejb.ConcurrencyManagement;
 import jakarta.ejb.ConcurrencyManagementType;
+import jakarta.ejb.Schedule;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,11 +25,13 @@ import java.util.concurrent.ConcurrentMap;
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class IteratorHolder {
 
-    private final ConcurrentMap<UUID, AutoCloseableIterator<?>> content = new ConcurrentHashMap<>(10);
+    private final ConcurrentMap<UUID, TimeStamped<AutoCloseableIterator<?>>> content = new ConcurrentHashMap<>(10);
 
+    private final int TIMEOUT_SECONDS = 2 * 60; // 2 minutes
+    
     public synchronized <T> UUID add(final AutoCloseableIterator<T> iter) {
         final UUID uuid = UUID.randomUUID();
-        content.put(uuid, iter);
+        content.put(uuid, new TimeStamped<>(iter));
         return uuid;
     }
 
@@ -32,7 +41,50 @@ public class IteratorHolder {
 
     @SuppressWarnings("unchecked")
     public <T> AutoCloseableIterator<T> get(UUID uuid) {
-        return (AutoCloseableIterator<T>) content.get(uuid);
+        if (!content.containsKey(uuid)) {
+            return null;
+        }
+        return (AutoCloseableIterator<T>) content.get(uuid).getContent();
+    }
+
+    @Schedule(hour = "*", minute = "*", second = "5", persistent = false)
+    public void timeout() {
+        Set<UUID> toRemove = new HashSet<>();
+        
+        for (Map.Entry<UUID, TimeStamped<AutoCloseableIterator<?>>> me : content.entrySet()) {
+            TimeStamped<AutoCloseableIterator<?>> ts = me.getValue();
+                long sessionIdleTime = (System.currentTimeMillis() - ts.getLastAccessed()) / 1000;
+                if (sessionIdleTime > TIMEOUT_SECONDS) {
+                    Logger.getLogger(Sessions.class.getPackage().getName()).log(Level.INFO, "Timeout exceeded ({0} sec), closing iterator.", new Object[]{TIMEOUT_SECONDS});
+                    toRemove.add(me.getKey());
+                    ts.getContent().close(); // closes underlying db connection
+                }
+        }
+
+        for (UUID uuid : toRemove) {
+            content.remove(uuid);
+        }
+    }
+
+    private static class TimeStamped<T> {
+
+        private final T content;
+        private long lastAccess;
+
+        public TimeStamped(T content) {
+            this.content = content;
+            lastAccess = System.currentTimeMillis();
+        }
+
+        public T getContent() {
+            lastAccess = System.currentTimeMillis();
+            return content;
+        }
+
+        public long getLastAccessed() {
+            return lastAccess;
+        }
+
     }
 
 }
